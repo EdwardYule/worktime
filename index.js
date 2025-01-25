31358123a61c16216dbb59001cb0866e30588904
Edward <787322353@qq.com>
2025-01-25 12:13:51 +0800

    feat(index): 增加了对特定作者的提交记录进行统计的功能
    - 在 gitCommand 中添加了 --author 参数，仅统计 Edward 的提交记录
    - 重构了 execRepo 函数，使用 Promise 处理异步操作
    - 使用 Promise.all 并行处理所有仓库的提交记录
    - 添加了对提交记录进行过滤和统计的逻辑
    - 优化了输出格式，增加了每个仓库的提交次数和占比

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 const { exec, execSync } = require('child_process');
 const { getDate, findGitRepositories } = require('./utils');
+const path = require('path');
 
 const { since, until } = getDate();
-const gitCommand = `git log --since="${since}" --until="${until}" --oneline --pretty=format:%cd --date=format:%d`;
-const execRepo = (repo) => {
-    exec(gitCommand, { cwd: repo }, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`执行 Git 命令时出错: ${error.message}`);
-            execSync(`git config --global --add safe.directory ${repo}`);
-            return;
-        }
-        if (stderr) {
-            console.error(`Git 命令输出错误信息: ${stderr}`);
-            return;
-        }
-        const dateList = [...new Set(stdout.split('\n'))];
-        console.log(`${repo}: ${dateList} ${dateList[0]} length: ${dateList.length}`);
-    });
+const gitCommand = `git log --since="${since}" --until="${until}" --oneline --pretty=format:%cd --date=format:%d --author=Edward`;
+const execRepo = (cwd) => {
+    return new Promise((resolve, reject) => {
+        exec(gitCommand, { cwd }, (error, stdout, stderr) => {
+            const repo = path.resolve(__dirname, rootDirectory, cwd).replace(/\\/g, '/');
+            if (error) {
+                console.error(`${repo} 执行 Git 命令时出错: ${error.message}`);
+                execSync(`git config --global --add safe.directory ${repo}`);
+                reject(error);
+                return;
+            }
+            if (stderr) {
+                console.error(`Git 命令输出错误信息: ${stderr}`);
+                reject(stderr);
+                return;
+            }
+            const dateList = [...new Set(stdout.split('\n').filter(e => e !== ''))];
+            resolve({repo, dateList, length: dateList.length});
+        });
+    })
 }
 
 const rootDirectory = '../Edward';
 const gitRepos = findGitRepositories(rootDirectory);
-gitRepos.forEach(repo => {
-    execRepo(repo);
+Promise.all(gitRepos.map(repo => execRepo(repo))).then((res) => {
+    const result = res.filter(e => e.dateList.length > 0);
+    const total = result.reduce((a, b) => a + b.length, 0);
+    result.forEach(e => console.log(e.repo, e.dateList, e.length, `${Math.round((e.length / total) * 10000) / 100}%`));
+    // console.log(total, result)
+    console.log('All done!')
 });
